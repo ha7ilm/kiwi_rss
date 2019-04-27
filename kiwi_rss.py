d33269289439bf05d5ff10fc9ebe348b97e7b78d
ha7ilm <ha7ilm@sdr.hu>
2019-04-27 18:02:02 +0200

    Added small tk gui, warnings, -S

--- kiwi_rss.py
+++ kiwi_rss.py
@@ -10,6 +10,7 @@ import logging
 import socket
 import time
 import signal
+from Tkinter import *
 from datetime import datetime
 
 import wsclient
@@ -34,20 +35,16 @@ rss_thread = None
 signal.signal(signal.SIGINT, signal_handler)
 
 parser = OptionParser()
-parser.add_option("-s", "--server", type=str,
-                  help="server name", dest="server", default='192.168.1.82')
-parser.add_option("-p", "--port", type=int,
-                  help="port number", dest="port", default=8073)
-parser.add_option("-z", "--zoom", type=int,
-                  help="zoom factor", dest="zoom", default=0)
-parser.add_option("-o", "--offset", type=int,
-                  help="start frequency in kHz", dest="start", default=0)
-parser.add_option("-v", "--verbose", type=int,
-                  help="whether to print progress and debug info", dest="verbosity", default=0)
-parser.add_option("-n", "--no-listen", action="store_true",
-                  help="whether to disable listening for RSS", dest="no-listen", default=False)
-parser.add_option("-w", "--plot-waterfall", action="store_true",
-                  help="whether to plot the waterfall data using matplotlib", dest="plot-waterfall", default=False)
+parser.add_option("-s", "--server", type=str, help="server name", dest="server", default='192.168.1.82')
+parser.add_option("-p", "--port", type=int, help="port number", dest="port", default=8073)
+parser.add_option("-z", "--zoom", type=int, help="zoom factor", dest="zoom", default=0)
+parser.add_option("-o", "--offset", type=int, help="start frequency in kHz", dest="start", default=0)
+parser.add_option("-S", "--speed", type=int, help="waterfall speed", dest="speed", default=3)
+parser.add_option("-v", "--verbose", type=int, help="whether to print progress and debug info", dest="verbosity", default=0)
+parser.add_option("-n", "--no-listen", action="store_true", help="whether to disable listening for RSS", dest="no-listen", default=False)
+parser.add_option("-w", "--plot-waterfall", action="store_true", help="whether to plot the waterfall data using matplotlib", dest="plot-waterfall", default=False)
+parser.add_option("--waterfall-lower", action="store_true", 
+        help="whether to use the lower part of the waterfall", dest="waterfall-lower", default=False)
 
 options = vars(parser.parse_args()[0])
 
@@ -94,7 +91,7 @@ def rss_worker():
     print "Waiting for RSS to connect on TCP port 8888..."
     rss_conn, rss_addr = rss_socket.accept()
     print "RSS connected from address: ", rss_addr
-    cmd = "F %d|S %d|O %d|C 512|\r\n"%(1.5*center_freq*1e3, 0.5*full_span*1e3, 0)
+    cmd = "F %d|S %d|O %d|C 512|\r\n"%((0.5 if options["waterfall-lower"] else 1.5)*center_freq*1e3, 0.5*full_span*1e3, 0)
     print "Sending command:\n"+cmd
     rss_conn.send(cmd)
     while True:
@@ -142,7 +139,7 @@ print "Data stream active..."
 # send a sequence of messages to the server, hardcoded for now
 # max wf speed, no compression
 msg_list = ['SET auth t=kiwi p=', 'SET zoom=%d start=%d'%(zoom,offset),\
-'SET maxdb=0 mindb=-100', 'SET wf_speed=4', 'SET wf_comp=0']
+'SET maxdb=0 mindb=-100', 'SET wf_speed=%d'%(options["speed"]), 'SET wf_comp=0']
 for msg in msg_list:
     mystream.send_message(msg)
 print "Starting to retrieve waterfall data..."
@@ -157,7 +154,22 @@ if plt:
 
 last_keepalive = 0
 
+tk_root = Tk()
+tk_root.title("kiwi_rss.py")
+value_offset=DoubleVar(value=120)
+Scale(tk_root, from_=-50, to=200, variable=value_offset, label="Offset").pack(anchor=CENTER)
+
+value_mult=DoubleVar(value=1)
+Scale(tk_root, from_=0., to=3., variable=value_mult, label="Gain", resolution=0.1).pack(anchor=CENTER)
+
+log_enable = IntVar(value=1)
+Radiobutton(tk_root, text="Logarithmic scale", variable=log_enable, value=1).pack(anchor=W)
+Radiobutton(tk_root, text="Linear scale", variable=log_enable, value=0).pack(anchor=W)
+Label(text="Y = Gain * (X + Offset)").pack(anchor=W)
+
 while True:
+    tk_root.update_idletasks()
+    tk_root.update()
     #sys.stdout.write("O")
     if time.time()-last_keepalive > 1:
         mystream.send_message("SET keepalive")
@@ -181,19 +193,29 @@ while True:
             plt.draw()
             plt.pause(0.01)
 
-        rss_wf_data=wf_data[512:]
+        rss_wf_data=wf_data[512:] if not options["waterfall-lower"] else wf_data[:511]
         #rss_wf_data=4095+rss_wf_data
-        #rss_wf_data=4096*(10**((rss_wf_data+80)/20))
-        rss_wf_data=(rss_wf_data+120)*(4096/60.)
+        if log_enable.get()>0:
+            rss_wf_data=(rss_wf_data+value_offset.get())*(4096/60.)*value_mult.get()
+        else:
+            rss_wf_data=value_mult.get()*4096*(10**((rss_wf_data+value_offset.get())/20))
+        rss_wf_too_high = 0
+        rss_wf_too_low = 0
         for key in range(len(rss_wf_data)):
-            if rss_wf_data[key] > 4095: rss_wf_data[key] = 4095
-            elif rss_wf_data[key] < 0: rss_wf_data[key] = 0
-        #print rss_wf_data
+            if rss_wf_data[key] > 4095: 
+                rss_wf_data[key] = 4095
+                rss_wf_too_high += 1
+            elif rss_wf_data[key] < 0: 
+                rss_wf_data[key] = 0
+                rss_wf_too_low += 1
+        if rss_wf_too_high or rss_wf_too_low:
+            print "warning: values clamped, %d bin(s) above value 4095, %d bin(s) below value 0"%(rss_wf_too_high, rss_wf_too_low)
         rss_wf_data=np.flip(rss_wf_data)
 
         if rss_enable: 
             rss_queue.put(struct.pack(">%dH"%rss_wf_data.size, *rss_wf_data)+"\xfe\xfe")
-            #if qsize>0: print "qsize:", rss_queue.qsize()
+            qsize =  rss_queue.qsize()
+            if qsize>10: print "warning: rss transmit queue size =", qsize,"> 10"
             if rss_thread_finished[0]: break
 
     else: # this is chatter between client and server
@@ -205,4 +227,3 @@ try:
     mysocket.close()
 except Exception as e:
     print "exception: %s" % e
-
